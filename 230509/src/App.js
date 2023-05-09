83bc7702859295c6f42527632098ea0b21cc01ad
holaleche <106927728+withLeche@users.noreply.github.com>
2023-05-09 16:23:20 +0900

    React
    1. 이벤트 사용하기
    2. 상태 관리 (useState)

---
+++ 230509/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Login from "./Login";
+import Homepage from "./Homepage";
+import Modal from "./Modal";
+
+function Hello(props) {
+  const name = props.name;
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //1~10호를 만들 것입니다.
+
+  // list map을 이용하여 h1 태그들의 리스트를 만듭니다.
+
+  return (
+    <div>
+      {num.map((i) => (
+        <h1>
+          안녕, {name} {i}호
+        </h1>
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const user = {
+    idUser: "jaehyun@weniv.com",
+    pwUser: "1234",
+  };
+
+  const [login, setLogin] = useState(false);
+  const [modalShow, setModalShow] = useState(true);
+
+  function modalClose() {
+    setModalShow(false);
+  }
+
+  console.log(login);
+  return (
+    <>
+      <Hello name="licat" />
+      {login ? <Homepage /> : <Login infoUser={user} setLogin={setLogin} />}
+      {modalShow && (
+        <Modal modalClose={modalClose}>
+          <h2>사용약관에 대해 말씀 드립니다.</h2>
+          <p>...</p>
+          <a href="#none">더 보기</a>
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ 230509/src/Modal.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./Modal.css";
+
+export default function Modal({ children, modalClose }) {
+  return (
+    <div className="modal-backdrop">
+      <article className="modal">
+        {children}
+        {/* {<h2>홈페이지에 오신것을 환영합니다!</h2>
+        <p>좋은하루 되세요!</p>} */}
+        <button onClick={modalClose}>닫기</button>
+      </article>
+    </div>
+  );
+}
